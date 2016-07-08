1aa773475091f58ceb53c2c5dfd6643ad5b1187c
atungare <ajay.tungare@gmail.com>
2016-07-08 17:45:11 -0400

    fahr to cels

---
+++ ch1/fahr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* Prints Fahrenheit to Celsius tables
+    for fahr/cels in (0,20,...,300) */
+
+main()
+{
+  float fahr, cels;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  printf("Fahrenheit to Celsius\n");
+  fahr = lower;
+  while (fahr <= upper) {
+    cels = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, cels);
+    fahr = fahr + step;
+  }
+
+  printf("Celsius to Fahrenheit\n");
+  cels = lower;
+  while (cels <= upper) {
+    fahr = ((9.0 / 5.0) * cels) + 32.0;
+    printf("%3.0f %6.1f\n", cels, fahr);
+    cels = cels + step;
+  }
+}
